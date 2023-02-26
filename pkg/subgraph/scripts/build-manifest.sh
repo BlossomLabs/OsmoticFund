ae769ab2f82beecbbecdd074e4a7cb06e52bb520
PJColombo <paulo.colombo@pm.me>
2023-02-26 23:22:58 +0100

    feat(subgraph): track `MimeTokenFactory` contract

---
+++ pkg/subgraph/scripts/build-manifest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NETWORK=$1
+
+if [ "$STAGING" ]
+then
+  FILE=$NETWORK'-staging.json'
+else
+  FILE=$NETWORK'.json'
+fi
+
+DATA=manifest/data/$FILE
+
+echo 'Generating manifest from data file: '$DATA
+cat $DATA
+
+# -p manifest/templates/contracts/OsmoticController.template.yaml \
+# -p manifest/templates/contracts/OsmoticPool.template.yaml \
+# -p manifest/templates/contracts/ProjectRegistry.template.yaml \
+
+mustache \
+  -p manifest/templates/sources/MimeTokenFactories.yaml \
+  -p manifest/templates/contracts/MimeTokenFactory.template.yaml \
+  $DATA \
+  subgraph.template.yaml > subgraph.yaml
