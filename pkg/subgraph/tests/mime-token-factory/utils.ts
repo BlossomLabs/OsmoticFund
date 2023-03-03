9f5eba29f9c025e7b26f6b0027511cf21327b1d6
PJColombo <paulo.colombo@pm.me>
2023-03-04 00:02:16 +0200

    test(subgraph): add the following:
    - Set up `matchstick`
    - Add mime token factory tests
    
    Note: In order for `matchstick` to work with a pnpm workspace, the `assemblyscript` dep needed to be added explictly as a workaround. The graph cli doesn't detect it when running the tests (probably because of the way pnpm handles deps through symbolic links)

---
+++ pkg/subgraph/tests/mime-token-factory/utils.ts
@@ -0,0 +1,15 @@
+import { newMockEvent } from 'matchstick-as'
+
+import { MimeTokenCreated as MimeTokenCreatedEvent } from "../../generated/MimeTokenFactory@0.0.1/MimeTokenFactory";
+import { getAddressEventParam } from '../converters'
+
+export function createMimeTokenCreatedEvent(token: string): MimeTokenCreatedEvent {
+  const mockEvent = newMockEvent()
+  // @ts-ignore
+  const mimeTokenCreatedEvent = changetype<MimeTokenCreatedEvent>(newMockEvent())
+  
+  mimeTokenCreatedEvent.parameters = new Array()
+  mimeTokenCreatedEvent.parameters.push(getAddressEventParam('token', token))
+
+  return mimeTokenCreatedEvent
+}
---
+++ pkg/subgraph/tests/mocked-functions.ts
@@ -0,0 +1,71 @@
+import { Address } from "@graphprotocol/graph-ts";
+import { createMockedFunction } from "matchstick-as";
+
+import { getETHInt32, getETHString } from "./converters";
+
+export function mockedTokenRPCCalls(
+  address: string,
+  // @ts-ignore
+  decimals: i32,
+  name: string,
+  symbol: string
+): void {
+  mockedTokenDecimals(address, decimals);
+  mockedTokenName(address, name);
+  mockedTokenSymbol(address, symbol)
+}
+
+/**
+ * Creates a mocked token.name() function
+ * @param tokenAddress
+ * @param expectedName
+ */
+export function mockedTokenName(
+  tokenAddress: string,
+  expectedName: string
+): void {
+  createMockedFunction(
+    Address.fromString(tokenAddress),
+    "name",
+    "name():(string)"
+  )
+    .withArgs([])
+    .returns([getETHString(expectedName)]);
+}
+
+/**
+ * Creates a mocked token.symbol() function
+ * @param tokenAddress
+ * @param expectedSymbol
+ */
+export function mockedTokenSymbol(
+  tokenAddress: string,
+  expectedSymbol: string
+): void {
+  createMockedFunction(
+    Address.fromString(tokenAddress),
+    "symbol",
+    "symbol():(string)"
+  )
+    .withArgs([])
+    .returns([getETHString(expectedSymbol)]);
+}
+
+/**
+ * Creates a mocked token.decimals() function
+ * @param tokenAddress
+ * @param expectedDecimals
+ */
+export function mockedTokenDecimals(
+  tokenAddress: string,
+  // @ts-ignore
+  expectedDecimals: i32
+): void {
+  createMockedFunction(
+    Address.fromString(tokenAddress),
+    "decimals",
+    "decimals():(uint8)"
+  )
+    .withArgs([])
+    .returns([getETHInt32(expectedDecimals)]);
+}
