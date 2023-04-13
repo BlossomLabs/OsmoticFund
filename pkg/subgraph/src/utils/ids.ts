e4883a8a060e458dafa3654b8facb82e2c04725a
0xGabi <gabrielpk.18@gmail.com>
2023-04-13 00:21:07 -0300

    fix: update abis, include new mimetoken factory data, and update graph-cli

--- pkg/subgraph/src/utils/ids.ts
+++ pkg/subgraph/src/utils/ids.ts
@@ -2,7 +2,6 @@ import { Address } from "@graphprotocol/graph-ts";
 
 export const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
 
-
 export function formatAddress(address: Address): string {
   return address.toHexString().toLowerCase();
 }
