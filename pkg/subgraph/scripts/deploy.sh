8e84654f321c42196d57419713a4d349ee3ea986
0xGabi <gabrielpk.18@gmail.com>
2023-04-15 18:44:15 -0300

    feat(subgraph): add goerli deployment

---
+++ pkg/subgraph/scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Arguments
+USER=$1
+NAME=$2
+NETWORK=$3
+
+# Build manifest
+echo ''
+echo '> Building manifest file subgraph.yaml'
+./scripts/build-manifest.sh $NETWORK
+
+# Generate types
+echo ''
+echo '> Generating types'
+graph codegen
+
+# Prepare subgraph name
+FULLNAME=$USER/$NAME-$NETWORK
+if [ "$STAGING" ]; then
+  FULLNAME=$FULLNAME-staging
+fi
+echo ''
+echo '> Deploying subgraph: '$FULLNAME
+
+# Deploy subgraph
+graph deploy $FULLNAME \
+  --node https://api.thegraph.com/deploy/ \
+  --access-token $GRAPHKEY
