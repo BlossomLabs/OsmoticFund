8921ba70c4e9fbfed3d1f746b8e8566fa17c5d67
PJColombo <paulo.colombo@pm.me>
2023-04-21 01:07:40 +0200

    chore: fix web app TS config + remove unused `ui` package

--- apps/web/src/pages/create-pool/index.tsx
+++ apps/web/src/pages/create-pool/index.tsx
@@ -14,16 +14,15 @@ import {
   SliderFilledTrack,
   SliderThumb,
   VStack,
-} from '@chakra-ui/react'
-import TopBar from '../../components/Topbar'
+} from "@chakra-ui/react";
+import TopBar from "../../components/Topbar";
 
 type CreatePoolPage = {
-    id: string
-}
+  id: string;
+};
 
-export const CreatePoolPage = ({id}: CreatePoolPage) => {
-
-  const handleSubmit = (e) => {
+export const CreatePoolPage = ({ id }: CreatePoolPage) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     // Handle form submission
   };
@@ -32,18 +31,13 @@ export const CreatePoolPage = ({id}: CreatePoolPage) => {
     <Stack justify="center" align="center" spacing="30px">
       <TopBar />
       <Stack maxWidth="1200" px="8" mx="auto">
-        <Heading size="lg">
-          Create new Pool
-        </Heading>
+        <Heading size="lg">Create new Pool</Heading>
         <Text>You can give support to multiple projects at the same time</Text>
         <Box as="form" onSubmit={handleSubmit} w="full">
           <VStack spacing={4}>
             <FormControl id="pool-name">
               <FormLabel>Pool name</FormLabel>
-              <Input
-                type="text"
-                placeholder="Which is your organization?"
-              />
+              <Input type="text" placeholder="Which is your organization?" />
             </FormControl>
 
             <FormControl id="pool-image">
@@ -53,9 +47,7 @@ export const CreatePoolPage = ({id}: CreatePoolPage) => {
 
             <FormControl id="pool-description">
               <FormLabel>Pool description</FormLabel>
-              <Textarea
-                placeholder="What projects are you targeting? Why should they receive money from this pool?"
-              />
+              <Textarea placeholder="What projects are you targeting? Why should they receive money from this pool?" />
             </FormControl>
 
             <FormControl id="governance-token">
@@ -84,14 +76,11 @@ export const CreatePoolPage = ({id}: CreatePoolPage) => {
 
             <FormControl id="max-streaming-per-month">
               <FormLabel>Max streaming per month</FormLabel>
-              <Input
-                type="text"
-                placeholder="Examples: 20000 or 4%"
-              />
+              <Input type="text" placeholder="Examples: 20000 or 4%" />
             </FormControl>
 
             <Button type="submit" colorScheme="blue">
-          Submit
+              Submit
             </Button>
           </VStack>
         </Box>
---
+++ apps/web/src/pages/pool/[id].tsx
@@ -0,0 +1,87 @@
+import { Stack, Heading, Image, Text, Flex } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { ProjectCard } from "~/components/ProjectCard";
+import { usePools } from "~/hooks/usePools";
+import { useProjects } from "~/hooks/useProjects";
+import TopBar from "../../components/Topbar";
+
+export const PoolPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { pools } = usePools();
+  const { projects } = useProjects();
+  const pool = pools?.find((pool) => pool.name === id) || {
+    name: null,
+    description: null,
+    supporting: 0,
+    govToken: null,
+    elegibleProjects: [],
+    streaming: 0,
+    streamed: 0,
+    token: null,
+    streams: {},
+    available: 0,
+  };
+  const elegibleProjects =
+    pool.elegibleProjects.map((name: string) =>
+      projects.find((project) => project.name === name)
+    ) || [];
+  return (
+    <Stack justify="center" align="center" spacing="30px">
+      <TopBar />
+
+      <Stack mt={"0px !important"} width="100%">
+        <Image
+          src={`/img/${pool.name}.png`}
+          alt={pool.name ?? ""}
+          height="300px"
+          objectFit="cover"
+        />
+      </Stack>
+
+      <Stack maxWidth="1200" px="8" mx="auto">
+        <Flex direction={{ base: "column", md: "row" }}>
+          <Stack pr="8" mb="16" flex={1}>
+            <Heading size="lg">{pool.name}</Heading>
+
+            <Text align="justify">{pool.description}</Text>
+          </Stack>
+          <Stack minWidth="300px">
+            <Heading size="sm">Governance</Heading>
+            <Text>
+              {pool.supporting.toLocaleString()} {pool.govToken} supporting
+            </Text>
+            <Heading size="sm">Streaming</Heading>
+            <Text>
+              {pool.streaming.toLocaleString()} {pool.token}/mo of{" "}
+              {pool.available.toLocaleString()} {pool.token}
+            </Text>
+            <Heading size="sm">Total streamed</Heading>
+            <Text>
+              {pool.streamed.toLocaleString()} {pool.token}
+            </Text>
+          </Stack>
+        </Flex>
+        <Heading size="md">Elegible Projects</Heading>
+        <Stack direction={{ base: "column", md: "row" }}>
+          {elegibleProjects.map((project) => {
+            if (!project) return null;
+            const res = pool.streams;
+            return (
+              <ProjectCard
+                projectName={project.name}
+                description={project.description}
+                //@ts-ignore //TODO: remove
+                streaming={pool.streams[project.name].streaming as number}
+                //@ts-ignore //TODO: remove
+                streamed={pool.streams[project.name].streamed as number}
+              />
+            );
+          })}
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default PoolPage;
