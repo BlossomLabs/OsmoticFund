429dd94e3db3d14a6567a1ef6d47265b40d05aa3
Gabi <gabrielpk.18@gmail.com>
2023-11-03 22:17:47 -0300

    fix(subgraph): save funding token metadata

--- pkg/subgraph/src/utils/token.ts
+++ pkg/subgraph/src/utils/token.ts
@@ -1,5 +1,6 @@
 import { Address } from "@graphprotocol/graph-ts";
 
+import { MimeToken as MimeTokenContract } from "../../generated/OsmoticController/MimeToken";
 import { Token as TokenEntity } from "../../generated/schema";
 
 import { formatAddress } from "./ids";
@@ -14,6 +15,15 @@ export function loadOrCreateTokenEntity(tokenAddress: Address): TokenEntity {
   let token = TokenEntity.load(tokenId);
   if (token == null) {
     token = new TokenEntity(tokenId);
+
+    // we use MimeTokenContract to get the token's name, symbol and decimals
+    const tokenContract = MimeTokenContract.bind(tokenAddress);
+
+    token.name = tokenContract.name();
+    token.symbol = tokenContract.symbol();
+    token.decimals = tokenContract.decimals();
+
+    token.save();
   }
 
   return token;
